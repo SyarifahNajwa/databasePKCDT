cfd14c56bcabd42840efb98ce932887ce7165bfa
Zakiyya Humaira <zakiyya.h@mhs.usk.ac.id>
2026-06-02 12:04:50 +0700

    menghapus welcoming page

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Models\Penomoran;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Grup Route yang wajib Login
